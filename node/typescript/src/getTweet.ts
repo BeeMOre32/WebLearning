b0f7f02ab346ac408b6adb0647fdf9682cc22283
BeeMOre32 <windowssart01@gmail.com>
2024-05-12 21:54:52 +0900

    index.html 내용 변경 및 counter main 코드 제거

---
+++ node/typescript/src/getTweet.ts
@@ -0,0 +1,55 @@
+// https://jsonplaceholder.typicode.com/posts
+
+async function getUserById(id) {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    const data = await response.json();
+    return data;
+  }
+
+async function getTweets() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+  
+    const tweetContainer = document.querySelector(".main-tweet");
+    const tweetData = data.splice(0, 10);
+  
+    console.log(tweetData);
+  
+    const userIds = Array.from(new Set(tweetData.map((tweet) => tweet.userId)));
+    const users = await Promise.all(userIds.map((id) => getUserById(id)));
+  
+    const result = tweetData.map((tweet) => {
+      const user = users.find((user) => user.id === tweet.userId);
+  
+      return `
+            <div class="tweet">
+                <div class="tweet-header">
+                    <div class="tweet-info">
+                        <div class="user-photo">
+                        <i class="fas fa-user"></i>
+                        </div>
+                        <div class="tweet-username">${user.username}</div>
+                        <div class="tweet-handle">@${user.username}</div>
+                        <div class="tweet-time">1h</div>
+                    </div>
+                </div>
+                <div class="tweet-content">
+                    ${tweet.body}
+                </div>
+                <div class="tweet-actions">
+                    <i class="far fa-comment"></i>
+                    <i class="fas fa-retweet"></i>
+                    <i class="far fa-heart"></i>
+                    <i class="fas fa-share"></i>
+                </div>
+            </div>
+        `;
+    });
+  
+    tweetContainer.innerHTML = result.join("");
+}
+
+getTweets();
+  
--- node/typescript/src/main.ts
+++ node/typescript/src/main.ts
@@ -1,25 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
 
 const number = 1;
 const string = "hello";
@@ -34,7 +13,4 @@ function sum(a: number, b: number): number {
 
 const result = sum(1, 2);
 // const resultError = sum(1, "2");
-
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 
