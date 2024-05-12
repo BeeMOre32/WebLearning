38b0dfa2842cd62c1bdae77e093c91132d6717aa
BeeMOre32 <windowssart01@gmail.com>
2024-05-12 18:19:40 +0900

    typescript 예제추가

---
+++ node/typescript/src/interface.ts
@@ -0,0 +1,17 @@
+interface Person {
+    name: string;
+    age: number;
+}
+    
+const person: Person = {
+    name: "Alice",
+    age: 30
+};
+
+
+const person2: {name: string; age: number;} = {
+    name: "Bob",
+    age: 25
+};
+
+
