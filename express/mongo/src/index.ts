1049593ec0848c9661e75772b0137e7439bbe126
BeeMOre32 <windowssart01@gmail.com>
2024-05-19 17:11:14 +0900

    mongoose connection

--- express/mongo/src/index.ts
+++ express/mongo/src/index.ts
@@ -1,7 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import tweetRouter from "./router/tweet";
-import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const url = process.env.MONGO_URL as string;
+mongoose.connect(url);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB with Mongoose");
+});
 
 const app: Express = express();
 const port = 3000;
@@ -17,29 +29,3 @@ app.listen(port, () => {
 });
 
 app.use(tweetRouter);
-
-dotenv.config();
-
-const uri = process.env.MONGO_URL || "";
-console.log("uri", uri);
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
